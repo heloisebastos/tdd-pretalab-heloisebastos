e3e16bdef8e1ccd23745b6616b40e81e85dea9fe
heloisebastos <heloisebastos28@hotmail.com>
2024-07-13 11:24:47 -0400

    :sparkles: feat: Adiciona solucao do desafio  pretalab

--- queue.js
+++ queue.js
@@ -1,10 +1,33 @@
 class Queue {
   elements
-  constructor(){
+
+  constructor() {
     this.elements = []
   }
-  
-  // Implementar aqui
+
+  adicionarElemento(element) {
+    this.elements.push(element)
+    return element
+  }
+
+  calcularTamanhoDaFila() {
+    return this.elements.length
+  }
+
+  buscarPrimeiroElemento() {
+    if (this.elements.length === 0) {
+      throw new Error("A lista está vazia ");
+    }
+    return this.elements[0];
+  }
+
+  removerPrimeiroItem() {
+    if (this.elements.length === 0) {
+      throw new Error("A fila esta vazia");
+    }
+    return this.elements.shift();
+  }
+
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -3,23 +3,39 @@ const Queue = require('./queue')
 describe('Queue', () => {
   it('Deve criar uma fila', () => {
     const queue = new Queue()
-
-    expect(queue).toEqual({ elements: []})
+    expect(queue).toEqual({ elements: [] })
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.adicionarElemento(1)
+    queue.adicionarElemento(2)
+    queue.adicionarElemento(3)
+    expect(queue.calcularTamanhoDaFila()).toEqual(3)
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+    queue.adicionarElemento(1)
+    expect(queue.calcularTamanhoDaFila()).toEqual(1)
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.adicionarElemento(1)
+    queue.adicionarElemento(2)
+    queue.adicionarElemento(3)
+    expect(queue.buscarPrimeiroElemento()).toEqual(1)
+
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.adicionarElemento(1)
+    queue.adicionarElemento(2)
+    queue.adicionarElemento(3)
+    expect(queue.removerPrimeiroItem()).toEqual(1)
+    expect(queue.calcularTamanhoDaFila()).toEqual(2)
+    expect(queue.buscarPrimeiroElemento()).toEqual(2)
   })
 })
